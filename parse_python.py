4fe7a72001c369e687bd6f03d6f024fb5dadcd5e
P K Agrawal <pranshukumar1999@gmail.com>
2021-07-27 21:16:42 +0530

    Comments Added

--- parse_python.py
+++ parse_python.py
@@ -1,14 +1,21 @@
 import xml.etree.ElementTree as ET
+#from lxml import etree as ET
 
+#Get the XML File data
 stream = open('sample_xml.xml','r')
 
+#Parse the data into an ElementTree Object
 xml = ET.parse(stream)
 
+#Get the 'root' Element object from the ElementTree
 root = xml.getroot()
 
+#Iterate through each child of the root element
 for e in root:
 
+    #Print the stringified version of the element
     print(ET.tostring(e))
     print("")
-
+    
+    #Print the 'Id' attribute of the Element objeect
     print(e.get("Id"))

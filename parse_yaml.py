f967fc9595b6d305aabeeedd57dd25374f666c8a
P K Agrawal <pranshukumar1999@gmail.com>
2021-07-28 10:17:46 +0530

    Parsing YAML using python implemented

--- parse_yaml.py
+++ parse_yaml.py
@@ -4,7 +4,10 @@ from yaml import load, load_all
 stream = open('sample.yaml', 'r')
 data = load_all(stream, Loader = yaml.FullLoader)
 
+print(type(data))
+
 for doc in data:
+    print(type(doc))
     print("New Document:")
     for key, value in doc.items():
         print(key + ": "+ str(value))

c3d3f2f02dda0fdf399b3dd8059def9e92547f6e
P K Agrawal <pranshukumar1999@gmail.com>
2021-07-28 09:41:51 +0530

    Comments Added

--- parse_xmltodict.py
+++ parse_xmltodict.py
@@ -1,7 +1,9 @@
 import xmltodict
 
+#Get the XML File
 stream = open('sample_xml.xml','r')
 
+#Parse the XML file into an OrderedDictionary
 xml = xmltodict.parse(stream.read())
 
 for e in xml["People"]["Person"]:
